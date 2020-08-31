c21fb5ca206f5a434436c0cc5fb1b1ec0d385361
Thomas Murray <thomas.murray09@gmail.com>
2020-08-31 16:31:15 +0200

    Webapp

--- app.rb
+++ app.rb
@@ -2,11 +2,28 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
+  @usernames = [ 'ssaunier', 'Papillard' ]
   erb :index
+end
+
+get '/people' do
+  erb :people
+end
+
+get '/layout' do
+  erb :layout
+end
+
+get '/team/:username' do
+  # binding.pry
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
